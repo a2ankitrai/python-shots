d14a3cf9dce18d14ff6079a6dc1d2f31a4cb00dc
Ankit Rai <a2.ankitrai@gmail.com>
2022-07-10 03:33:23 +0200

    try catch

---
+++ _07_try_except.py
@@ -0,0 +1,23 @@
+
+def division_func(var1, var2):
+    try:
+        int_var1 = int(var1)
+        int_var2 = int(var2)
+
+        print('Performing Division operation')
+        res = int_var1 / int_var2
+
+        print(f'result for operation: {res}')
+    except ValueError:
+        print('Error: Invalid input provided')
+    except ZeroDivisionError:
+        print('Error: Dont divide by zero')
+    except:  # catch all error
+        print('Generic error handling')
+
+
+print('Welcome to Division program unit')
+num1 = input('Enter first number (Numerator)\n')
+num2 = input('Enter second number (Denominator)\n')
+
+division_func(num1, num2)
