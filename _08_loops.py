9af6b49b2d98b67f5364843e8cc8c5b758c7bee4
Ankit Rai <a2.ankitrai@gmail.com>
2022-07-11 21:17:28 +0200

    loops and list

--- _08_loops.py
+++ _08_loops.py
@@ -1,8 +1,28 @@
+# while loop
 def loop_func(seed):
     num = seed
-    while num < 100:
+    while num < 10:
         num = num + 1
         print(num)
 
 
-loop_func(3)
+# loop_func(3)
+
+#  ---
+# for loop
+
+my_list = [1, 2, 3, 5, 8]
+
+for val in my_list:
+    double = int(val) * 2
+    print(double)
+print('first for loop finished')
+
+my_list = '100, 200, 300'
+print(type(my_list.split(',')))
+print(my_list.split(','))
+
+for val in my_list.split(','):
+    double = int(val) * 2
+    print(double)
+print('loop finished')
---
+++ _09_lists.py
@@ -0,0 +1,18 @@
+fruits = ['Apple', 'Banana', 'Cherry']
+
+print(fruits[1])  # Banana
+print(fruits)
+
+# add elements
+fruits.append("Mango")
+print(fruits)
+
+# remove all elements
+fruits.clear()
+print(fruits)
+
+# sort elements
+fruits = ['Cherry', 'Apple', 'Banana']
+print(fruits)
+fruits.sort()
+print(fruits)
